ae018d77788d929084e00b52d82beb94af5ed722
Pikkel <ilovemathguy@gmail.com>
2022-02-09 01:48:36 -0500

    made it check for the version

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread, time};
+use std::{io::Read, process::Command, thread, time};
 use webbrowser;
 
 // TODO: add something to open a terminal to update and after to install the lowlatency debs
@@ -12,8 +12,16 @@ use webbrowser;
 #[allow(unused_must_use)]
 fn main() {
     loop {
-        // wondering if I can make it download from https://kernel.ubuntu.com/~kernel-ppa/mainline/?C=N;O=D automatically (even if I write it in a stupid way)
-        webbrowser::open("https://kernel.ubuntu.com/~kernel-ppa/mainline/?C=N;O=D");
-        thread::sleep(time::Duration::from_secs(86400));
+        // wondering if I can make it download from https://kernel.ubuntu.com/ automatically (even if I write it in a stupid way)
+        Command::new("bash")
+            .arg("./getkernel")
+            .spawn()
+            .expect("failed step 0");
+        let mut kernver = std::fs::File::open("/tmp/kernver-latest").unwrap();
+        let mut contents = String::new();
+        kernver.read_to_string(&mut contents).unwrap();
+        webbrowser::open(&*("https://kernel.ubuntu.com/~kernel-ppa/mainline/v".to_owned() + &contents));
+        thread::sleep(time::Duration::from_secs(5));
+        // thread::sleep(time::Duration::from_secs(86400));
     }
 }
