8dc59d16b7fbff574dce8a883c17337de50eba4c
Pikkel <ilovemathguy@gmail.com>
2022-02-09 04:12:27 -0500

    made it kinda better but some stuff needs to be fixed

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
-use std::{io::Read, process::Command, thread, time};
+use std::{fs, io::Read, io::Write, process::Command, thread, time};
 use webbrowser;
 
 // TODO: remove the webbrowser part and make it download and hopefully install the pkgs
-/*
-    linux-headers-VERSION-NUMBER_all.deb
-    linux-headers-VERSION-NUMBER_amd64.deb
-    linux-image-VERSION-NUMBER_amd64.deb
-    linux-modules-VERSION-NUMBER_amd64.deb
-*/
 
 #[allow(unused_must_use)]
 fn main() {
+    fs::create_dir_all("/tmp/.kernver/");
     loop {
-        // wondering if I can make it download from https://kernel.ubuntu.com/ automatically (even if I write it in a stupid way)
+        // curl -s https://kernel.ubuntu.com/\~kernel-ppa/mainline/v5.16.8/amd64/ | grep -A1 'href=' | grep -oP '(?<=deb">).*(?=lowlatency).*(?=</a.*)'
+        // curl -s https://kernel.ubuntu.com/\~kernel-ppa/mainline/v5.16.8/amd64/ | grep -A1 'href=' | grep -oP '(?<=all.deb">).*(?=</a.*)'
         Command::new("bash")
             // command from here >> https://stackoverflow.com/questions/25078065/how-to-check-latest-kernel-version-by-bash
             .arg("./getkernel")
             .spawn()
             .expect("failed step 0");
-        let mut kernver = std::fs::File::open("/tmp/kernver-latest").unwrap();
-        let mut contents = String::new();
-        kernver.read_to_string(&mut contents).unwrap();
-        webbrowser::open(&*("https://kernel.ubuntu.com/~kernel-ppa/mainline/v".to_owned() + &contents));
+        let mut kernver = std::fs::File::open("/tmp/.kernver/kern-latest").unwrap();
+        let mut ver = String::new();
+        kernver.read_to_string(&mut ver).unwrap();
+        // TODO: make the commands not go into 4 lines
+        let mut file =
+            std::fs::File::create("/tmp/.kernver/downloadkern").expect("failed to create");
+        file.write_all(("curl -s https://kernel.ubuntu.com/\\~kernel-ppa/mainline/v".to_owned() + &ver + "/amd64/ | grep -A1 'href=' | grep -oP '(?<=deb\">).*(?=lowlatency).*(?=</a.*)'\n").as_bytes())
+            .expect("failed to write config");
+        file.write_all(
+            ("curl -s https://kernel.ubuntu.com/\\~kernel-ppa/mainline/v".to_owned()
+                + &ver
+                + "/amd64/ | grep -A1 'href=' | grep -oP '(?<=all.deb\">).*(?=</a.*)'")
+                .as_bytes(),
+        )
+        .expect("failed to write config");
+        webbrowser::open(&*("https://kernel.ubuntu.com/~kernel-ppa/mainline/v".to_owned() + &ver));
         thread::sleep(time::Duration::from_secs(5));
         // thread::sleep(time::Duration::from_secs(86400));
     }
