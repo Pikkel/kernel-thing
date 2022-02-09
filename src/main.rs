cf189ceebaa6d5408e404cde585ebe7fa1a238ac
Pikkel <76259034+Pikkel@users.noreply.github.com>
2022-02-09 08:57:57 -0500

    trying to make it not require files to be created before execution

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,32 @@ use webbrowser;
 fn main() {
     fs::create_dir_all("/tmp/.kernver/");
     loop {
-        // curl -s https://kernel.ubuntu.com/\~kernel-ppa/mainline/v5.16.8/amd64/ | grep -A1 'href=' | grep -oP '(?<=deb">).*(?=lowlatency).*(?=</a.*)'
-        // curl -s https://kernel.ubuntu.com/\~kernel-ppa/mainline/v5.16.8/amd64/ | grep -A1 'href=' | grep -oP '(?<=all.deb">).*(?=</a.*)'
+        // curl -s https://kernel.org | grep -A1 'stable:' | grep -oP '(?<=strong>).*(?=</strong.*)' > /tmp/.kernver/kern-latest
+        let mut getkernver = std::fs::File::create("/tmp/.kernver/getkernver").expect("failed to create `/tmp/.kernver/getkernver`");
+        getkernver.write_all("curl -s https://kernel.org | grep -A1 'stable:' | grep -oP '(?<=strong>).*(?=</strong.*)' > /tmp/.kernver/kern-latest".as_ref()).expect("failed to write to `/tmp/.kernver/getkernver`");
         Command::new("bash")
             // command from here >> https://stackoverflow.com/questions/25078065/how-to-check-latest-kernel-version-by-bash
-            .arg("./getkernel")
+            .arg("/tmp/.kernver/getkernver")
             .spawn()
-            .expect("failed step 0");
+            .expect("failed to execute `/tmp/.kernver/getkernver`");
         let mut kernver = std::fs::File::open("/tmp/.kernver/kern-latest").unwrap();
         let mut ver = String::new();
         kernver.read_to_string(&mut ver).unwrap();
         // TODO: make the commands not go into 4 lines
-        let mut file =
-            std::fs::File::create("/tmp/.kernver/downloadkern").expect("failed to create");
-        file.write_all(("curl -s https://kernel.ubuntu.com/\\~kernel-ppa/mainline/v".to_owned() + &ver + "/amd64/ | grep -A1 'href=' | grep -oP '(?<=deb\">).*(?=lowlatency).*(?=</a.*)'\n").as_bytes())
-            .expect("failed to write config");
-        file.write_all(
+        // curl -s https://kernel.ubuntu.com/\~kernel-ppa/mainline/v5.16.8/amd64/ | grep -A1 'href=' | grep -oP '(?<=deb">).*(?=lowlatency).*(?=</a.*)'
+        // curl -s https://kernel.ubuntu.com/\~kernel-ppa/mainline/v5.16.8/amd64/ | grep -A1 'href=' | grep -oP '(?<=all.deb">).*(?=</a.*)'
+        let mut downloadkern =
+            std::fs::File::create("/tmp/.kernver/downloadkern").expect("failed to create `/tmp/.kernver/downloadkern`");
+        downloadkern.write_all(("curl -s https://kernel.ubuntu.com/\\~kernel-ppa/mainline/v".to_owned() + &ver + "/amd64/ | grep -A1 'href=' | grep -oP '(?<=deb\">).*(?=lowlatency).*(?=</a.*)'\n").as_bytes())
+            .expect("failed to write to `/tmp/.kernver/downloadkern`");
+        downloadkern.write_all(
             ("curl -s https://kernel.ubuntu.com/\\~kernel-ppa/mainline/v".to_owned()
                 + &ver
                 + "/amd64/ | grep -A1 'href=' | grep -oP '(?<=all.deb\">).*(?=</a.*)'")
                 .as_bytes(),
         )
-        .expect("failed to write config");
-        webbrowser::open(&*("https://kernel.ubuntu.com/~kernel-ppa/mainline/v".to_owned() + &ver));
+        .expect("failed to write to `/tmp/.kernver/downloadkern`");
+        // webbrowser::open(&*("https://kernel.ubuntu.com/~kernel-ppa/mainline/v".to_owned() + &ver));
         thread::sleep(time::Duration::from_secs(5));
         // thread::sleep(time::Duration::from_secs(86400));
     }
